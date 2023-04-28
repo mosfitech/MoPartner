bd7cff86a0091d7055ac915a42381779ede72ffd
fandilladp <fandilla.118130122@student.itera.ac.id>
2023-04-28 09:46:02 +0700

    calculate vehicle

--- src/components/DetailRent.tsx
+++ src/components/DetailRent.tsx
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import DirectionsBikeIcon from "@mui/icons-material/DirectionsBike";
 export default function DetailRent(props: any) {
+  const mapBetween = (
+    currentNum: any,
+    minAllowed: any,
+    maxAllowed: any,
+    min: any,
+    max: any
+  ) => {
+    return (
+      ((maxAllowed - minAllowed) * (currentNum - min)) / (max - min) +
+      minAllowed
+    );
+  };
   return (
     <div className="bg-primary-500 dark:bg-gray-800 shadow-lg rounded-md flex items-center justify-between p-3 border-b-4 border-blue-600 dark:border-gray-600 text-white flex items-center  justify-between p-4  rounded-lg shadow-indigo-50 shadow-md mx-2">
       <div className="w-44">
@@ -13,6 +25,9 @@ export default function DetailRent(props: any) {
         <p className="text-sm font-semibold text-gray-400">
           Status :{" "}
           {props.data.rental_status === 0 ? "Ready to rent" : "Rental On Going"}
+        </p>
+        <p className="text-sm font-semibold text-gray-400">
+          battray : {mapBetween((props.data.battray * 10), 0, 100, 33, 50).toFixed(2)} %
         </p>
         <div className="text-xs mt-6 px-1 py-2 bg-yellow-400 text-white rounded-lg  tracking-wider hover:bg-yellow-300 outline-none">
           Last updated : {props.data.updated_at}
